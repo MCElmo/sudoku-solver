87cdab53cb76ca1e6e01b20d6c58013f044f392a
Seth Setse <seth@sethsetse.com>
2023-07-08 05:02:23 -0700

    UI done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,29 @@
+"use client"
+
 import Image from 'next/image'
+import { useEffect, useState } from 'react'
 
 export default function Home() {
+
+  const [board,setBoard] = useState<(number | null)[][]>([[]])
+
+  useEffect(() => {
+    let newBoard: number | null[][] = []
+    for (let i = 0; i < 9; i++) {
+        let row: number | null[] = []
+        for (let j = 0; j < 9; j++) {
+            row.push(null)
+        }
+        newBoard.push(row)
+    }
+    setBoard(newBoard)
+  },[])
+
+  const setBoardCell = (row: number, col: number, value: number | null) => {
+    board[row][col] = value
+    setBoard([...board])
+  }
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
@@ -28,86 +51,68 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div style={{ flex: 1, position: 'relative'}} className='z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex'>
+        <div className='board'>
+            {
+                board.map((row,y) => {
+                    return row.map((item,x) => (
+                        <div key={`${x}-${y}`} className='board-item' contentEditable onInput={(e) => {
+                            const key = Number(e.nativeEvent.data)
+                            if (Number.isNaN(key) || key < 1 || key > 9) {
+                                setBoardCell(y,x,null)
+                            }else {
+                                setBoardCell(y,x,key)
+                            }
+                        }}>
+                            {item}
+                        </div>
+                    ))
+                })
+            }
+            <div style={{
+                position: 'absolute',
+                left: "calc(100% / 3)",
+                height: "100%",
+                width: 3,
+                backgroundColor: "black"
+            }}  />
+            <div style={{
+                position: 'absolute',
+                right: "calc(100% / 3)",
+                height: "100%",
+                width: 3,
+                backgroundColor: "black"
+            }}  />
+            <div style={{
+                position: 'absolute',
+                top: "calc(100% / 3)",
+                width: "100%",
+                height: 3,
+                backgroundColor: "black"
+            }}  />
+            <div style={{
+                position: 'absolute',
+                bottom: "calc(100% / 3)",
+                width: "100%",
+                height: 3,
+                backgroundColor: "black"
+            }}  />
+        </div>
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <button style={{
+        backgroundColor: "gold",
+        color: "white",
+        border: "none",
+        outline: "none",
+        cursor: "pointer",
+        padding: "10px 20px",
+      }} onClick={() => {
+        console.log(board)
+      }}>
+        Solve
+      </button>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   )
 }
